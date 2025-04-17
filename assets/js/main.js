61b0f849df647868bcea9d188b3331bb00ce2345
Ashish Makwana <85098548+ashishmakwana96am@users.noreply.github.com>
2025-04-17 17:24:08 +0530

    code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     const sponsorCols = document.querySelectorAll('.show-modal-overlay');
-  
-//     sponsorCols.forEach((sponsor) => {
-//       const triggerBtn = sponsor.querySelector('[data-bs-toggle="modal"]');
-  
-//       if (triggerBtn) {
-//         triggerBtn.addEventListener('click', function () {
-//           sponsorCols.forEach(col => col.classList.remove('active'));
-  
-//           sponsor.classList.add('active');
-  
-//           const modalId = triggerBtn.getAttribute('data-bs-target');
-//           const modalEl = document.querySelector(modalId);
-  
-//           if (modalEl) {
-//             modalEl.addEventListener('hidden.bs.modal', function () {
-//               sponsor.classList.remove('active');
-//             }, { once: true });
-//           }
-//         });
-//       }
-//     });
-//   });
  
 document.addEventListener('DOMContentLoaded', function () {
   const sponsorCols = document.querySelectorAll('.show-modal-overlay');
@@ -32,28 +8,19 @@ document.addEventListener('DOMContentLoaded', function () {
               sponsorCols.forEach(col => col.classList.remove('active'));
               sponsor.classList.add('active');
               const modalId = triggerBtn.getAttribute('data-bs-target');
-              console.log("modalId",modalId)
               const modalEl = document.querySelector(modalId);
-              console.log("modalEl",modalEl)
               if (modalEl) {
                   modalEl.addEventListener('hidden.bs.modal', function () {
                       sponsor.classList.remove('active');
                   }, { once: true });
 
-                  adjustModalPosition(modalEl, sponsor);
+                  adjustModalPosition( sponsor);
               }
           });
       }
   });
-  // function adjustModalPosition(modalEl, sponsor) {
-  //   console.log("sponsor",sponsor)
-  //     const sponsorRect = sponsor.getBoundingClientRect();
-  //     console.log("sponsorRect",sponsorRect)
-  //     const modalHeight = modalEl.offsetHeight;
-  //     const sponsorHeight = sponsor.offsetHeight;
-  //     modalEl.style.transform = `translateY(${sponsorRect.top - modalHeight - sponsorHeight}px)`;
-  // }
-  function adjustModalPosition(modalEl, sponsor) {
+
+  function adjustModalPosition( sponsor) {
     const sponsorRect = sponsor.getBoundingClientRect();
     const sponsorTop = sponsorRect.top + window.scrollY;
     const windowHeight = window.innerHeight;
